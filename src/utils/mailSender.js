f6d01496658b51b1fcc89a421d5d90ad4024299d
Aman Yadav <107180522+Aman8355@users.noreply.github.com>
2025-09-24 16:24:04 +0530

    Update mailSender.js

--- src/utils/mailSender.js
+++ src/utils/mailSender.js
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 
 export const sendOTPEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.hostinger.com",
-    port: 465,
+     service: "gmail",
+    // port: 465,
     secure: true,
     auth: {
       user: process.env.EMAIL_USER,
